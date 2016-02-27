6ab1b040ea0d3b0801c315e0e2c993c92a082c4b
Ryan Tremblay <zaptrem@gmail.com>
2016-02-27 15:45:41 -0500

    Initial Commit

---
+++ parkinsonsClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parkinsonsClient
+//
+//  Created by Ryan Tremblay on 2/27/16.
+//  Copyright © 2016 FP Computer Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
